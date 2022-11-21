7b81feb995ca7ed03e7319f510dde5b5fb1c27dc
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-11-21 16:19:26 -0500

    finish BE post + get

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const bodyParser = require('body-parser');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
 const app = express();
 
 // Basic Configuration
@@ -9,6 +12,8 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -19,11 +24,31 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const savedUrls = [];
+
 app.post('/api/shorturl', (req, res, next) => {
-  console.log(req.body);
-  next();
-  res.json({original_url: "", short_url: "" });
-  return next();
+  let inputUrl = req.body.url;
+  let urlObject = new URL(inputUrl);
+  let shortUrlNumber;
+  dns.lookup(urlObject.hostname, (err, address, family) => {
+    if(err){
+      res.json({ error: 'invalid url' });
+      return next();
+    }
+    else {
+      if (!savedUrls.includes(inputUrl)) {
+        savedUrls.push(inputUrl);
+      }
+      shortUrlNumber = savedUrls.indexOf(inputUrl);
+      res.json({original_url: inputUrl, short_url: shortUrlNumber });
+      return next();
+    }
+  });
+});
+
+app.get(`/api/shorturl/:short_url`, (req, res, next) => {
+  let redirectUrl = savedUrls[req.params.short_url];
+  res.redirect(redirectUrl);
 });
 
 app.listen(port, function() {
