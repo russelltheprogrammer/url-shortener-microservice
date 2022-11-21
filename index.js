db1687759b04301f9991a3515b21451bdf1f49a4
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-11-21 16:21:58 -0500

    remove random code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const dns = require('dns');
 const bodyParser = require('body-parser');
-const { hasUncaughtExceptionCaptureCallback } = require('process');
 const app = express();
 
 // Basic Configuration
